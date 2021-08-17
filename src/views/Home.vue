4177d8c7670dc1553599262681401ef471fd3a34
Aymane Benhima <aymanebenhima@gmail.com>
2021-08-17 14:41:24 +0100

    Add card to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home container">
+    <div class="card" v-for="keep in keeps" :key="keep.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ keep.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(item, index) in keep.ingredients" :key="index">
+            <span class="chip">{{ item }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      keeps: [
+        {
+          id: 1,
+          title: "Aymane Benhima",
+          slug: "aymane-benhima",
+          ingredients: ["front-end", "back-end"],
+        },
+        {
+          id: 2,
+          title: "Ouail Benhima",
+          slug: "ouail-benhima",
+          ingredients: ["css", "js"],
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style>
+.home {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+.home h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.home .ingredients {
+  margin: 30px auto;
+}
+.home .ingredients li {
+  display: inline-block;
+}
+</style>
